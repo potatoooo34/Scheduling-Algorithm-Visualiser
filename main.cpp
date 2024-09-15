807a344a95fcbee4ae6567bf3c452c05641777c7
Harsh Raj <108672710+potatoooo34@users.noreply.github.com>
2024-09-16 00:25:31 +0530

    changed the logic for tasks now a copy of tasks is given to each scheduler

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <queue>
 #include <chrono>
+#include <stdexcept>
 
 using namespace std;
 // create a class with pid , burst time and arrival time , priority
@@ -21,6 +22,8 @@ private:
     int burst_time;
     int rem_time;
     int priority;
+    int wait_time;
+    bool finished;
 
 public:
     Task(int pid, int at, int bt, int pr)
@@ -30,6 +33,8 @@ public:
         this->burst_time = bt;
         this->rem_time = bt;
         this->priority = pr;
+        this->wait_time = 0;
+        this->finished = false;
     }
     int getRemTime()
     {
@@ -59,61 +64,80 @@ public:
     {
         this->rem_time = time;
     }
+    void increasePriority(int change)
+    {
+        if (this->priority > 1)
+        {
+            this->priority -= change;
+        }
+    }
+    int getWaitTime()
+    {
+        return this->wait_time;
+    }
+
+    bool getFinished()
+    {
+        return this->finished;
+    }
+    void setFinished(bool cond)
+    {
+        this->finished = cond;
+    }
+    void setWaitingTime(int time)
+    {
+        this->wait_time = time;
+    }
 };
 class TaskManager
 {
 private:
-    unordered_map<int, Task *> tasks;
+    vector<Task> tasks;
 
 public:
-    TaskManager(vector<Task *> taskslist)
+    TaskManager(vector<Task> taskslist)
     {
-        for (auto &task : taskslist)
-        {
-            tasks[task->getPid()] = task;
-        }
+        this->tasks = taskslist;
     }
     ~TaskManager()
     {
-        for (auto &pair : tasks)
-        {
-            delete pair.second;
-        }
         tasks.clear();
     }
-    void addTasks(Task *task)
+    void addTasks(Task task)
     {
-        tasks[task->getPid()] = task;
+        tasks[task.getPid()] = task;
     }
-    vector<Task *> getTasks()
+    vector<Task> getTasks()
     {
-        vector<Task *> task;
-        for (auto &t : tasks)
-        {
-            task.push_back(t.second);
-        }
-        return task;
-    }
-    Task *getTaskById(int id)
-    {
-        if (tasks.find(id) != tasks.end())
-        {
-            return tasks[id];
-        }
 
-        return nullptr;
+        return tasks;
+    }
+    Task getTaskById(int id)
+    {
+        bool found = false;
+
+        for (auto it : tasks)
+        {
+            if (it.getPid() == id)
+            {
+                found = true;
+                return it;
+            }
+        }
+        if (found == false)
+            throw runtime_error("Task not found");
     }
 };
 class RoundRobin : public Schedular
 {
 private:
-    TaskManager &tm;
+    TaskManager tm;
     int timequantum;
     vector<double> arrival;
     vector<double> finish;
 
 public:
-    RoundRobin(TaskManager &tm, int tq) : tm(tm)
+    RoundRobin(TaskManager tm, int tq) : tm(tm)
     {
 
         this->timequantum = tq;
@@ -126,49 +150,50 @@ public:
     {
 
         int n = tm.getTasks().size();
-        vector<Task *> tasks = tm.getTasks();
-        sort(tasks.begin(), tasks.end(), [](Task *a, Task *b)
-             { return a->getArrivalTime() < b->getArrivalTime(); });
-        queue<Task *> q;
+        vector<Task> tasks = tm.getTasks();
+        sort(tasks.begin(), tasks.end(), [](Task a, Task b)
+             { return a.getArrivalTime() < b.getArrivalTime(); });
+        queue<Task> q;
 
         int curr_time = 0;
         int i = 0;
 
         while (!q.empty() || i < n)
         {
-            while (i < n && tasks[i]->getArrivalTime() <= curr_time)
+            while (i < n && tasks[i].getArrivalTime() <= curr_time)
             {
-                arrival[tasks[i]->getPid() - 1] = tasks[i]->getArrivalTime();
+                arrival[tasks[i].getPid() - 1] = tasks[i].getArrivalTime();
                 q.push(tasks[i]);
                 i++;
             }
 
             if (!q.empty())
             {
-                Task *t = q.front();
+                Task t = q.front();
                 q.pop();
 
-                int time_executed = min(t->getRemTime(), this->timequantum);
+                int time_executed = min(t.getRemTime(), this->timequantum);
                 curr_time += time_executed;
-                t->decreaseRemTime(time_executed);
-                cout << "Process id " << t->getPid() << " Executed for " << time_executed
-                     << " ms Remaining time " << t->getRemTime() << ". Current Time " << curr_time << endl;
-                if (t->getRemTime() > 0)
+                t.decreaseRemTime(time_executed);
+                cout << "Process id " << t.getPid() << " Executed for " << time_executed
+                     << " ms Remaining time " << t.getRemTime() << ". Current Time " << curr_time << endl;
+                if (t.getRemTime() > 0)
                 {
                     q.push(t);
                 }
                 else
                 {
-                    cout << "Process id " << t->getPid() << " Finished at " << curr_time << endl;
-                    t->setRemTime(t->getBurstTime());
-                    finish[t->getPid() - 1] = curr_time;
+                    cout << "Process id " << t.getPid() << " Finished at " << curr_time << endl;
+                    t.decreaseRemTime(t.getBurstTime());
+                    finish[t.getPid() - 1] = curr_time;
                 }
+                // ageTasks(curr_time);
             }
             else
             {
                 if (i < n)
                 {
-                    curr_time = tasks[i]->getArrivalTime();
+                    curr_time = tasks[i].getArrivalTime();
                 }
             }
         }
@@ -186,17 +211,25 @@ public:
         cout << "Avg turnaround time: " << sum / double(n) << endl
              << endl;
     }
+    void ageTasks(int curr_time)
+    {
+        vector<Task> tasks = tm.getTasks();
+
+        for (auto task : tasks)
+        {
+        }
+    }
 };
 
 class ShortestJobFirst : public Schedular
 {
 private:
-    TaskManager &tm;
+    TaskManager tm;
     vector<double> arrive;
     vector<double> finish;
 
 public:
-    ShortestJobFirst(TaskManager &tm) : tm(tm)
+    ShortestJobFirst(TaskManager tm) : tm(tm)
     {
 
         int n = tm.getTasks().size();
@@ -206,18 +239,18 @@ public:
 
     void Schedule() override
     {
-        vector<Task *> tasks = tm.getTasks();
-        sort(tasks.begin(), tasks.end(), [](Task *a, Task *b)
-             { return a->getBurstTime() < b->getBurstTime(); });
+        vector<Task> tasks = tm.getTasks();
+        sort(tasks.begin(), tasks.end(), [](Task a, Task b)
+             { return a.getBurstTime() < b.getBurstTime(); });
         int curr_time = 0;
         for (auto &it : tasks)
         {
-            arrive[it->getPid() - 1] = it->getArrivalTime();
-            curr_time = max(curr_time, it->getArrivalTime());
-            cout << "Process id " << it->getPid()
-                 << " Executed For " << it->getBurstTime() << " ms. Current Time " << curr_time << endl;
-            curr_time += it->getBurstTime();
-            finish[it->getPid() - 1] = it->getArrivalTime() + it->getBurstTime();
+            arrive[it.getPid() - 1] = it.getArrivalTime();
+            curr_time = max(curr_time, it.getArrivalTime());
+            cout << "Process id " << it.getPid()
+                 << " Executed For " << it.getBurstTime() << " ms. Current Time " << curr_time << endl;
+            curr_time += it.getBurstTime();
+            finish[it.getPid() - 1] = it.getArrivalTime() + it.getBurstTime();
         }
     }
 
@@ -238,20 +271,21 @@ public:
 class Compare
 {
 public:
-    bool operator()(Task *a, Task *b)
+    bool operator()(Task a, Task b)
     {
-        return a->getPriority() > b->getPriority();
+        return a.getPriority() > b.getPriority();
     }
 };
 // lower number higher priority
 class PriorityScheduling : public Schedular
 {
-    TaskManager &taskmanager;
+    TaskManager taskmanager;
     vector<double> arrive;
     vector<double> finish;
+    int time_interval;
 
 public:
-    PriorityScheduling(TaskManager &tm) : taskmanager(tm)
+    PriorityScheduling(TaskManager tm) : taskmanager(tm)
     {
 
         int n = taskmanager.getTasks().size();
@@ -261,10 +295,10 @@ public:
 
     void Schedule() override
     {
-        vector<Task *> tasks = taskmanager.getTasks();
-        priority_queue<Task *, vector<Task *>, Compare> pq;
-        sort(tasks.begin(), tasks.end(), [](Task *a, Task *b)
-             { return a->getArrivalTime() < b->getArrivalTime(); });
+        vector<Task> tasks = taskmanager.getTasks();
+        priority_queue<Task, vector<Task>, Compare> pq;
+        sort(tasks.begin(), tasks.end(), [](Task a, Task b)
+             { return a.getArrivalTime() < b.getArrivalTime(); });
 
         int idx = 0;
         int curr_time = 0;
@@ -272,43 +306,46 @@ public:
         int time_executed = 0;
         while (!pq.empty() || idx < n)
         {
-            while (idx < n && curr_time >= tasks[idx]->getArrivalTime())
+            while (idx < n && curr_time >= tasks[idx].getArrivalTime())
             {
-                arrive[tasks[idx]->getPid() - 1] = tasks[idx]->getArrivalTime();
+                arrive[tasks[idx].getPid() - 1] = tasks[idx].getArrivalTime();
                 pq.push(tasks[idx]);
                 idx++;
             }
+
+            // ageTasks(curr_time);
             if (!pq.empty())
             {
                 auto m = pq.top();
                 pq.pop();
                 int time_Start = curr_time;
-                while (m->getRemTime() > 0)
+                while (m.getRemTime() > 0)
                 {
-                    if (idx < n && tasks[idx]->getArrivalTime() <= curr_time && tasks[idx]->getPriority() < m->getPriority())
+                    if (idx < n && tasks[idx].getArrivalTime() <= curr_time && tasks[idx].getPriority() < m.getPriority())
                     {
                         break; // highr priority process arrived;
                     }
                     curr_time++;
-                    m->decreaseRemTime(1);
+                    m.decreaseRemTime(1);
                 }
-                cout << "Process id " << m->getPid()
+                cout << "Process id " << m.getPid()
                      << " Executed For " << curr_time - time_Start << " ms. Current Time " << curr_time << endl;
-                if (m->getRemTime() > 0)
+                if (m.getRemTime() > 0)
                 {
                     pq.push(m);
                 }
                 else
                 {
-                    cout << "Process id " << m->getPid() << " Finished at " << curr_time << endl;
-                    finish[m->getPid() - 1] = curr_time;
-                    m->setRemTime(m->getBurstTime());
+                    cout << "Process id " << m.getPid() << " Finished at " << curr_time << endl;
+                    m.setFinished(true);
+                    finish[m.getPid() - 1] = curr_time;
+                    // m.setRemTime(m.getRemTime());
                 }
             }
             else
             {
                 if (idx < n)
-                    curr_time = tasks[idx]->getArrivalTime();
+                    curr_time = tasks[idx].getArrivalTime();
             }
         }
     }
@@ -321,113 +358,41 @@ public:
         {
             sum += finish[i] - arrive[i];
         }
-        cout << "Average TAT " << sum / (double)n << endl
-             << endl;
-    }
-};
-
-class MultiLevelQueue : public Schedular
-{
-private:
-    queue<Task *> highest;
-    queue<Task *> middle;
-    queue<Task *> lower;
-
-    TaskManager &taskmanager;
-    int timequantum;
-
-public:
-    MultiLevelQueue(int timeq, TaskManager &tm) : taskmanager(tm)
-    {
-        this->timequantum = timeq;
+        cout << "Average TAT " << sum / (double)n << endl;
     }
 
-    void Schedule() override
+    void ageTasks(int curr_time , Task curr_task)
     {
-        vector<Task *> tasks = taskmanager.getTasks();
-        sort(tasks.begin(), tasks.end(), [](Task *a, Task *b)
-             { return a->getArrivalTime() < b->getArrivalTime(); });
+        vector<Task> tasks = taskmanager.getTasks();
 
-        int curr_time = 0, idx = 0;
-        int n = tasks.size();
-
-        while (idx < n && curr_time <= tasks[idx]->getArrivalTime())
+        for (auto task : tasks)
         {
-            int priority = tasks[idx]->getPriority();
+            if(task.getRemTime() > 0){
 
-            if (priority == 1)
-            {
-                highest.push(tasks[idx]);
-            }
-            else if (priority > 1 && priority <= 3)
-            {
-                middle.push(tasks[idx]);
-            }
-            else
-            {
-                lower.push(tasks[idx]);
-            }
-
-            if (!highest.empty())
-            {
-                while (idx < n && tasks[idx]->getArrivalTime() <= curr_time)
-                {
-                    if(tasks[idx]->getPriority() == 1){
-                        highest.push(tasks[idx]);
-                    }
-                    else if(tasks[idx]->getPriority() > 1 && tasks[idx]->getPriority() <= 3){
-                        middle.push(tasks[idx]);
-                    }
-                    else{
-                        lower.push(tasks[idx]);
-                    }
-                    idx++;
+                if(task.getPid() == curr_task.getPid() || task.getFinished() == true){
+                    continue;
                 }
 
                 
-                    Task *t = q.front();
-                    q.pop();
 
-                    int time_executed = min(t->getRemTime(), this->timequantum);
-                    curr_time += time_executed;
-                    t->decreaseRemTime(time_executed);
-                    cout << "Process id " << t->getPid() << " Executed for " << time_executed
-                         << " ms Remaining time " << t->getRemTime() << ". Current Time " << curr_time << endl;
-                    if (t->getRemTime() > 0)
-                    {
-                        q.push(t);
-                    }
-                    else
-                    {
-                        cout << "Process id " << t->getPid() << " Finished at " << curr_time << endl;
-                        t->setRemTime(t->getBurstTime());
-                        
-                    }
-                }
-                else
-                {
-                    if (idx < n)
-                    {
-                        curr_time = tasks[i]->getArrivalTime();
-                    }
-                }
-            
+            }
         }
     }
 };
+
 int main()
 {
-    vector<Task *> tasks = {
-        new Task(1, 4, 5, 3),
-        new Task(2, 2, 3, 1),
-        new Task(3, 0, 8, 2),
-        new Task(4, 10, 2, 4),
-        new Task(5, 6, 4, 2),
-        new Task(6, 11, 6, 5),
-        new Task(7, 12, 7, 1),
-        new Task(8, 10, 3, 3),
-        new Task(9, 13, 5, 4),
-        new Task(10, 18, 4, 2)};
+    vector<Task> tasks = {
+        Task(1, 4, 5, 3),
+        Task(2, 2, 3, 1),
+        Task(3, 0, 8, 2),
+        Task(4, 10, 2, 4),
+        Task(5, 6, 4, 2),
+        Task(6, 11, 6, 5),
+        Task(7, 12, 7, 1),
+        Task(8, 10, 3, 3),
+        Task(9, 13, 5, 4),
+        Task(10, 18, 4, 2)};
 
     TaskManager tm(tasks);
 
@@ -451,11 +416,6 @@ int main()
     ShortestJobFirst sjf(tm);
     sjf.Schedule();
     sjf.CalculateMetrics();
-
-    for (auto task : tasks)
-    {
-        delete task;
-    }
 
     return 0;
 }
